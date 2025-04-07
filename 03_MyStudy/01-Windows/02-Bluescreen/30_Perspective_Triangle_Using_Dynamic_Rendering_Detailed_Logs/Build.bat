6a4144c480f57bdbdc864e195c0fc262141b8110
AnanthC099 <ananthchandrasekharan099@gmail.com>
2025-04-07 14:14:47 +0530

    30_Perspective_Triangle_Using_Dynamic_Rendering_Detailed_Logs
    30_Perspective_Triangle_Using_Dynamic_Rendering_Detailed_Logs

--- 03_MyStudy/01-Windows/02-Bluescreen/30_Perspective_Triangle_Using_Dynamic_Rendering_Detailed_Logs/Build.bat
+++ 03_MyStudy/01-Windows/02-Bluescreen/30_Perspective_Triangle_Using_Dynamic_Rendering_Detailed_Logs/Build.bat
@@ -6,11 +6,11 @@ glslc -fshader-stage=vert vert_shader.glsl -o vert_shader.spv
 
 glslc -fshader-stage=frag frag_shader.glsl -o frag_shader.spv
 
-cl /EHsc /I"C:\VulkanSDK\Vulkan\Include" /c Vk.cpp /Fo"Vk.obj"
+cl /EHsc /I"C:\VulkanSDK\Anjaneya\Include" /c Vk.cpp /Fo"Vk.obj"
 
 rc.exe Vk.rc
 
-link Vk.obj Vk.res /LIBPATH:"C:\VulkanSDK\Vulkan\Lib" vulkan-1.lib user32.lib gdi32.lib kernel32.lib /OUT:Vk.exe 
+link Vk.obj Vk.res /LIBPATH:"C:\VulkanSDK\Anjaneya\Lib" vulkan-1.lib user32.lib gdi32.lib kernel32.lib /OUT:Vk.exe 
 
 del Vk.obj Vk.pdb Vk.res
 
