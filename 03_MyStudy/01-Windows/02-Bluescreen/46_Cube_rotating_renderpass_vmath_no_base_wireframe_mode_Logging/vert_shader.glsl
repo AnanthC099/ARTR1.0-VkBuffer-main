86a56dd5edfc8c8c0f4fc0a4ca92549ab9c3264b
Ananth Chandrasekharan <ananthchandrasekharan099@gmail.com>
2025-04-11 02:21:26 +0530

    46_Cube_rotating_renderpass_vmath_no_base_wireframe_mode_Logging
    46_Cube_rotating_renderpass_vmath_no_base_wireframe_mode_Logging

---
+++ 03_MyStudy/01-Windows/02-Bluescreen/46_Cube_rotating_renderpass_vmath_no_base_wireframe_mode_Logging/vert_shader.glsl
@@ -0,0 +1,17 @@
+// vertex shader (vert_shader.glsl)
+#version 450
+
+layout(location = 0) in vec3 inPos;
+layout(location = 1) in vec3 inColor;
+
+layout(set = 0, binding = 0) uniform UBO {
+    mat4 mvp;
+} ubo;
+
+layout(location = 0) out vec3 outColor;
+
+void main()
+{
+    gl_Position = ubo.mvp * vec4(inPos, 1.0);
+    outColor = inColor;
+}
