b7883f0d727b90e1f1a0b42d61ee4c6079e18328
AnanthC099 <ananthchandrasekharan099@gmail.com>
2025-06-09 22:37:18 +0530

    29_Shader_as_string
    29_Shader_as_string

--- 01-Windows/03-Triangle/29_Shader_as_string/Build.bat
+++ 01-Windows/03-Triangle/29_Shader_as_string/Build.bat
@@ -2,11 +2,11 @@ cls
 
 del Vk.exe Log.txt
 
-cl /I"C:\VulkanSDK\Vulkan\Include" /c Vk.c /Fo"Vk.obj"
+cl /MD /std:c++17 /I"C:\VulkanSDK\Vulkan\Include" /I"C:\VulkanSDK\Vulkan\Include\glslang" /c Vk.cpp /Fo"Vk.obj"
 
 rc.exe Vk.rc
 
-link Vk.obj Vk.res /LIBPATH:"C:\VulkanSDK\Vulkan\Lib" vulkan-1.lib user32.lib gdi32.lib kernel32.lib /OUT:Vk.exe 
+link Vk.obj Vk.res /LIBPATH:"C:\VulkanSDK\Vulkan\Lib" vulkan-1.lib user32.lib gdi32.lib kernel32.lib  /OUT:Vk.exe 
 
 del Vk.obj Vk.pdb Vk.res
 
